ce7824e679a88db9d674d476abb1707d7bbfae1c
QuocDung21 <rongvang2357hn@gmail.com>
2023-10-02 02:30:15 +0700

    add

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -2,5 +2,6 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api'
   baseURL: "https://book-backend-ehmv.onrender.com/api",
+  withCredentials: true,
 });
 export default api;
