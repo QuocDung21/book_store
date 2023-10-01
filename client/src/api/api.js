6b3dbe18883a8edd431b1b717d041551a69f4d9a
QuocDung21 <rongvang2357hn@gmail.com>
2023-10-02 00:54:20 +0700

    add

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const local = "https://book-store-server-chi.vercel.app";
+const local = "https://book-backend-ehmv.onrender.com";
 //https://book-store-server-chi.vercel.app/
 const production = "";
 const api = axios.create({
