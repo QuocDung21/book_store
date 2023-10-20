2b11d9972cb3de30913980500ea7439244051399
QuocDung21 <rongvang2357hn@gmail.com>
2023-10-20 23:06:05 +0700

    fix

--- dashboard/src/views/seller/Profile.jsx
+++ dashboard/src/views/seller/Profile.jsx
@@ -171,25 +171,25 @@ const Profile = () => {
                     />
                   </div> */}
                   <div className="flex flex-col w-full gap-1 mb-3">
-                    <label htmlFor="district">District</label>
+                    <label htmlFor="district">Thành phố</label>
                     <input
                       value={state.district}
                       onChange={inputHandle}
                       className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]"
                       type="text"
-                      placeholder="district"
+                      placeholder="Thành phố"
                       name="district"
                       id="district"
                     />
                   </div>
                   <div className="flex flex-col w-full gap-1 mb-3">
-                    <label htmlFor="sub">Sub District</label>
+                    <label htmlFor="sub">Địa chỉ</label>
                     <input
                       value={state.sub_district}
                       onChange={inputHandle}
                       className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#283046] border border-slate-700 rounded-md text-[#d0d2d6]"
                       type="text"
-                      placeholder="sub district"
+                      placeholder="Địa chỉ"
                       name="sub_district"
                       id="sub"
                     />
